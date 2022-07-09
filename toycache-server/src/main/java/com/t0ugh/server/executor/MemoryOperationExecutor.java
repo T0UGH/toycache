29c681051bd4ab54296e67c29c771cdf2b12ec67
t0ugh <wang.g.p@foxmail.com>
2022-07-09 09:34:44 +0800

    fix: 无限等待队列

--- toycache-server/src/main/java/com/t0ugh/server/executor/MemoryOperationExecutor.java
+++ toycache-server/src/main/java/com/t0ugh/server/executor/MemoryOperationExecutor.java
@@ -6,6 +6,9 @@ import com.t0ugh.server.handler.Handler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 命令执行器, 命令都在一个单独的线程中执行
@@ -14,7 +17,8 @@ import java.util.concurrent.Executors;
 public class MemoryOperationExecutor extends AbstractMessageExecutor {
 
     public MemoryOperationExecutor(GlobalContext globalContext) {
-        super(globalContext, Executors.newSingleThreadExecutor());
+        super(globalContext, new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>(10000)));
     }
 
     @Override
